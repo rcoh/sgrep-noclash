8fb0e4635b51a488243d387f81be01afc277dd80
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-13 16:09:12 -0400

    change name of verison file, why is it null??

--- release-scripts/validate-osx-release.sh
+++ release-scripts/validate-osx-release.sh
@@ -8,15 +8,16 @@ brew install semgrep
 echo "Running homebrew recipe checks"
 brew test semgrep
 
-curl https://api.github.com/repos/returntocorp/sgrep/releases/latest | jq -r '.tag_name' | sed 's/^v//' > release-version
+curl https://api.github.com/repos/returntocorp/sgrep/releases/latest | jq -r '.tag_name' | sed 's/^v//' > version
+echo "Release version: $(cat version)"
 
 brew info semgrep --json | jq -r '.[0].installed[0].version' | tee brew-version
 
 semgrep --version > semgrep-version
 echo -n "Validating brew the version ($(cat brew-version) vs. $(cat release-version))..."
-diff brew-version release-version
+diff brew-version version
 echo "OK!"
 
 echo -n "Validating brew the version ($(cat semgrep-version) vs. $(cat release-version))..."
-diff semgrep-version release-version
+diff semgrep-version version
 echo "OK!"
